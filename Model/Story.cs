910280c25c7e872cb25423a0168cb86274d1270c
Huỳnh Ngọc Ý Nhi <21522423@gm.uit.edu.vn>
2024-06-03 15:51:55 +0700

    models

---
+++ Model/Story.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingApp.Model
+{
+    public class Story
+    {
+        public int StoryID { get; set; }
+
+        public string Image { get; set; }
+
+        [MaxLength(200)]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [MaxLength(100)]
+        public string Type { get; set; }
+
+        [MaxLength(50)]
+        public string Category { get; set; }
+
+        public bool IsPaid { get; set; } = false;
+
+        public int FreeChapters { get; set; } = 0;
+
+        public int Price { get; set; } = 0;
+
+        [MaxLength(100)]
+        public string Status { get; set; }
+
+        public User Author { get; set; }
+
+    }
+}
---
+++ Model/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingApp.Model
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool? IsMale { get; set; }
+        public DateTime? Dob { get; set; }
+    }
+}
